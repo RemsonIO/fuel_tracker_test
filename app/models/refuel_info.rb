b622f2909c8061ba9910cf76133dc6435354b0b3
RemsonIO <genevievejoshua@gmail.com>
2019-03-26 22:58:23 +0800

    refuel entity

---
+++ app/models/refuel_info.rb
@@ -0,0 +1,2 @@
+class RefuelInfo < ApplicationRecord
+end
---
+++ db/migrate/20190326144645_create_refuel_infos.rb
@@ -0,0 +1,13 @@
+class CreateRefuelInfos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :refuel_infos do |t|
+      t.datetime :date
+      t.string   :vehicle
+      t.integer  :odometer_reading
+      t.string   :refuel_location
+      t.integer  :liters_of_fuel
+
+      t.timestamps
+    end
+  end
+end
