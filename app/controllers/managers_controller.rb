2ed4139b5fc704654b381f0b32c24124fc46876e
RemsonIO <genevievejoshua@gmail.com>
2019-03-27 12:22:13 +0800

    current_user

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -7,8 +7,8 @@ class ManagersController < LoginsController
 
   private
   def authenticate_manager
-    return if @current_user.present? && @current_user.role == 'manager'
-    sign_out(@current_user)
+    return if current_user.present? && current_user.role == 'manager'
+    sign_out(current_user)
     redirect_to '/'
   end
 
